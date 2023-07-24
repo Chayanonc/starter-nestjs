0fd9930bcc3aaf778e1241bc898b3adb5b60783b
Chayanonc <chayanon@wiseboq.com>
2023-07-25 01:43:38 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  const port = process.env.PORT || configService.get<number>('PORT');
+  console.log(port);
+
+  app.enableCors();
+  await app.listen(port, () => {
+    console.log(`app is listen port: ${port}`);
+  });
 }
 bootstrap();
